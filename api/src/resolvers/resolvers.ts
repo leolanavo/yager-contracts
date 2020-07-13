dc3f4ec1e84f544da3a1eef22b66d69227529f52
Victor Martins João <victor_mjoao@hotmail.com>
2020-07-13 20:01:30 -0300

    Add create user mutation and interface

--- api/src/resolvers/resolvers.ts
+++ api/src/resolvers/resolvers.ts
@@ -1,28 +1,14 @@
 import { IResolvers } from 'apollo-server-koa';
-import { Context } from '@typings/Context';
 
 import { createCompany } from '@resolvers/Company';
 import { createPayment, getPayment } from '@resolvers/AppliedPayment';
-
+import { createUser } from '@resolvers/User';
 
 const resolvers: IResolvers = {
   Mutation: {
     createCompany,
     createPayment,
-    async createUser(_: any, args: any, context: Context, ___: any): Promise<any> {
-      const { neo4j }: Context = context;
-      const { name }: any = args;
-
-      const x = await neo4j.session
-        .run('MATCH (x:User) RETURN x;', {
-          name,
-        });
-
-      x.records.forEach(y => console.log(y.get('x')));
-
-      return "hello"
-    },
-  },
+    createUser,
   Query: {
     getPayment,
   }
