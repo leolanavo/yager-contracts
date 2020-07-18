27a39e959d47fa51a62bccd95c89ae099e6638f2
Victor Martins João <victor_mjoao@hotmail.com>
2020-07-17 23:52:28 -0300

    Fix addSignature

--- api/src/resolvers/User/addSignature.ts
+++ api/src/resolvers/User/addSignature.ts
@@ -2,6 +2,7 @@ import { ApolloError } from "apollo-server-koa";
 import { Context } from "@typings/Context";
 
 import User from "@postgres/models/User";
+import Company from "@postgres/models/Company";
 
 interface Args {
   userID: string;
@@ -12,19 +13,35 @@ export async function addSignature(
   _: any,
   args: Args,
   ___: Context,
-  ____: any,
+  ____: any
 ): Promise<any> {
   const { userID, signature } = args;
 
-  const user = await User.findOne({
-    id: userID
-  }, {
-    relations: ['party', 'company']
-  });
+  const user = await User.findOne(
+    {
+      id: userID,
+    },
+    {
+      relations: ["party", "companies"],
+    }
+  );
 
-  if (!user)
-    throw new ApolloError(`Could not find User with ${userID}`, '404');
+  if (!user) throw new ApolloError(`Could not find User with ${userID}`, "404");
 
+  const companies = await Promise.all(
+    user.companies.map((c) => {
+      return Company.findOne(
+        {
+          id: c.id,
+        },
+        {
+          relations: ["party"],
+        }
+      );
+    })
+  );
+
+  user.companies = companies as Company[];
   user.signature = signature;
   await User.save(user);
 
